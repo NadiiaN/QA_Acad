f51c43c18e2beb4fb5c45ea460d5711682469364
NadiiaN <36157211+NadiiaN@users.noreply.github.com>
2018-08-02 20:06:54 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,22 @@
+let values1 = [1, 3, 5, 7];
+let values2 = [2, 4, 6, 8];
+let sum = [];
+for (let i = 0; i < values1.length; i++) {
+    sum[i] = values1[i] + values2[i];
+}
+console.log(sum);
+/*console.log("\nNice work! Your first application have been launched.");
+console.log("Now, I need to know your name, type it below: ");
+
+let stdin = process.openStdin();
+
+stdin.addListener("data", (txt) => {
+    if(txt.toString().trim() === "quit") {
+        console.log("\nHave a nice JS trip! Bye-bye");
+        stdin.end();
+    } else {
+        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+    
+    }   
+  });*/
